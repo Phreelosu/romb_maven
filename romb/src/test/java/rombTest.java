88fbcfccec69aaac0a53485912ff802fa685770e
Phreelosu <siege.team22@gmail.com>
2024-01-10 10:49:54 +0100

    romb fix 6

--- romb/src/test/java/rombTest.java
+++ romb/src/test/java/rombTest.java
@@ -17,19 +17,19 @@ public class rombTest {
         double expected = 2704.3399073916876;
         Assert.assertEquals(actual, expected, 0.1, "Hiba! A megadott érték nem egyezik a várt értékkel!");
     }
-
+    @Test
     public void testCalcRange() {
         double actual2 = this.romb.calcRange(55.0);
         double expected2 = 220.0;
         Assert.assertEquals(actual2, expected2, 0.1, "Hiba! A megadott érték nem egyezik a várt értékkel!");
     }
-
+    @Test
     public void testCalcAreaFailForSure() {
         double actual3 = this.romb.calcArea(90.0);
         double expected3 = 1.0;
         Assert.assertEquals(actual3, expected3, 0.1, "Hiba! A megadott érték nem egyezik a várt értékkel!");
     } // ez nem fog igy mukodni de mindegy
-
+    @Test
     public void testCalcRangeFailForSure() {
         double actual4 = this.romb.calcRange(55.0);
         double expected4 = 1.0;
